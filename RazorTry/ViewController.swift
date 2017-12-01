3b5f80845e05a6cfec62f721cc126daf411ec9ae
SanamS <sanamsuri@sanams-mac-mini.local>
2017-12-01 20:23:11 +0530

    Initial Commit

---
+++ RazorTry/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RazorTry
+//
+//  Created by SanamS on 01/12/17.
+//  Copyright © 2017 SanamS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
