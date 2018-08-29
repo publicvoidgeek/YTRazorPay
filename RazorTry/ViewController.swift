e031c0a9a12826390fd9397500833ed215f43f3a
Sanam Suri <sanam@socialpeddler.com>
2018-08-29 14:22:45 +0530

    Working App

--- RazorTry/ViewController.swift
+++ RazorTry/ViewController.swift
@@ -7,19 +7,68 @@
 //
 
 import UIKit
+import Razorpay
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, RazorpayPaymentCompletionProtocol {
+   
+    
+    
+    //Outlets
+    @IBOutlet weak var errorLbl: UILabel!
+    @IBOutlet weak var paidLbl: UILabel!
+    
+    
+    var razorpay: Razorpay!
+    
+    let API_KEY = " "
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        razorpay = Razorpay.initWithKey(API_KEY, andDelegate: self)
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    
+    
+    func onPaymentError(_ code: Int32, description str: String) {
+        //Handle all the errors
+        errorLbl.isHidden = false
+        paidLbl.isHidden = true
     }
-
+    
+    func onPaymentSuccess(_ payment_id: String) {
+        //Handle all the success work here
+        errorLbl.isHidden = true
+        paidLbl.isHidden = false
+    }
+    
+    
+    @IBAction func payNowBtnPressed(_ sender: Any) {
+        showPaymentForm()
+        
+    }
+    
+    func showPaymentForm(){
+        let params: [String:Any] = [
+            "amount" : "10000",
+            "description" : "Sample Product",
+            "image" : "http://www.anblik.com/wp-content/uploads/2016/04/razorpay-logo.png",
+            "name" : "Public Void Geek",
+            "prefill" : [
+                "contact" : "9797979797",
+                "email" : "test@test.com"
+            ],
+            "theme" : [
+                "color" : "#F37254"
+            ]
+        ]
+        razorpay.open(params)
+        
+    }
+    
+    
 
 }
 
